3d7dbd47565953de612fb39a107817f13203854c
Your Name <you@example.com>
2024-06-01 18:21:26 +0800

    integration successful. pending: remove test cases and dummy screens and work on frontend UI

--- express/src/index.js
+++ express/src/index.js
@@ -1,57 +1,44 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser'); 
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const passport = require('passport');
-require('./strategies/local');
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const passport = require("passport");
+require("./strategies/local");
 
-const groceriesRoute = require('./routes/groceries'); 
-const marketsRoute = require('./routes/markets'); 
-const authRoute = require('./routes/auth');
+const authRoute = require("./routes/auth");
 
-require('./database'); 
+require("./database");
 
 const app = express();
 const PORT = 3001;
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(cookieParser());
-app.use(session({
-    secret: 'AHVSHJAVSDHGASCDAHGVSD', 
-    resave: false, 
-    saveUninitialized: false, 
-    store: MongoStore.create({ 
-        mongoUrl: 'mongodb://localhost:27017/express', 
-    }),
-
-})
+app.use(
+    session({
+        secret: "AHVSHJAVSDHGASCDAHGVSD",
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: "mongodb://localhost:27017/express",
+        }),
+    })
 );
-app.use((req, res, next) => { 
-    console.log(`${req.method}:${req.url}`); 
-    next(); 
-}); 
+app.use((req, res, next) => {
+    console.log(`${req.method}:${req.url}`);
+    next();
+});
 
-app.get('/ping', (request, response) => {
-    console.log('pong!');
-    response.send(200);
-})
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(passport.initialize()); 
-app.use(passport.session()); 
+app.get("", (req, res) => {
+    console.log("pong!");
+    res.send(200);
+});
 
 //register routes
-
-app.use('/api/groceries', groceriesRoute); 
-app.use('/api/markets', marketsRoute); 
-app.use('/api/auth', authRoute);
-
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}!`));
-
-
-
-
-
-
-
--- express/src/routes/auth.js
+++ express/src/routes/auth.js
@@ -4,26 +4,9 @@ const User = require('../database/Schemas/User');
 const { hashPassword , comparePassword } = require('../utils/helpers');
 const router = Router(); 
 
-// router.post('/login', async (request, response) => { 
-//     const { email, password } = request.body; 
-//     if (!email || !password) return response.send(400);
-//     const userDB = await User.findOne({ email }); 
-//     if(!userDB) return response.send(401); 
-//     const isValid = comparePassword(password, userDB.password); 
-//     if (isValid) { 
-//         console.log('Authenticated Successfully!');
-//         request.session.user = userDB; 
-//         return response.send(200); 
-//     } else { 
-//         console.log('Failed to Authenticate');
-//         return response.send(401); 
-//     }
-
-// }); //post request
-
 router.post('/login', passport.authenticate('local'), (req, res) => { 
-    console.log('Logged In'); 
-    res.send(200);
+    console.log(req.body.email + ' Logged In'); 
+    res.sendStatus(200);
 }); 
 
 router.post('/register', async(request, response) => { 
