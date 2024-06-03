6b4fa36b18c64034008b4c9bb00e1b8fade645e4
Your Name <you@example.com>
2024-06-03 13:17:17 +0800

    squashed bad commit in _layout.tsx. Probably the one causing the production app to crash.

--- HobbyLog/app/_layout.tsx
+++ HobbyLog/app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "expo-router";
+import { Stack } from "expo-router/stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native";
 
@@ -6,11 +6,7 @@ function RootLayout() {
     return (
         <SafeAreaProvider>
             <Stack screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="index" />
-                <Stack.Screen name="src/screens/HomeScreen/HomeScreen" />
-                <Stack.Screen name="src/screens/LoginScreen/LoginScreen" />
-                <Stack.Screen name="src/screens/SignUpScreen/SignUpScreen" />
-                <Stack.Screen name="src/screens/ForgotPasswordScreen/ForgotPasswordScreen" />
+                <Stack.Screen name="index.js" />
             </Stack>
         </SafeAreaProvider>
     );
